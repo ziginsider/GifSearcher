306050913cea4247949c216775368baf532b69b0
ziginsider <ziginsider@gmail.com>
2018-01-20 19:06:06 +0300

    add model SearchData

--- app/src/main/java/io/github/ziginsider/gifsearcher/model/SearchData.kt
+++ app/src/main/java/io/github/ziginsider/gifsearcher/model/SearchData.kt
@@ -4,4 +4,5 @@ package io.github.ziginsider.gifsearcher.model
  * Created by zigin on 20.01.2018.
  */
 class SearchData(val data: List<Gif>,
-                 )
+                 val meta: Meta,
+                 val pagination: Pagination)
